61252f95776796317f964dbae894eea5d329cc8e
Danil Uzlov <36223296+d-uzlov@users.noreply.github.com>
2023-12-29 05:10:21 +0700

    improve delete-failed-pods script

--- delete-failed-pods.sh
+++ delete-failed-pods.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 kl delete pods --field-selector status.phase=Failed --all-namespaces &
+p1=$!
 kl delete pods --field-selector status.phase=Succeeded --all-namespaces &
+p2=$!
+
+wait $p1 $p2
